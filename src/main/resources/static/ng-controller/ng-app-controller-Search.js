dd59d5de174160cc82e869900bc8cdcf92c99465
Lenovo <Lenovo@LENOVO-T420>
2017-08-31 11:42:51 +0200

    He incluido un archivo de lectura para probar el committ

--- src/main/resources/static/ng-controller/ng-app-controller-Search.js
+++ src/main/resources/static/ng-controller/ng-app-controller-Search.js
@@ -1,26 +1,31 @@
 app.controller('ng-app-controller-Search', ["$scope", "$http", "$timeout",
     function ($scope, $http, $timeout)
-    {   
-        $scope.search = function(){
-            var value= this.value;
-        $http.post('/searchItem',
-                {
-                    nombre: value
-                })
-                .then(function (response)
-                {
-                    var data = response.data;
-                    
-                    $scope.spring = data;
-                    $scope.testOk = true;
-                    
-                    $timeout(function()
+    {
+        $scope.search = function () {
+            var value = this.value;
+            $http.post('/searchItem',
                     {
-                     $scope.testOk = false;   
-                    }, 3000);
+                        nombre: value
+                    })
+                    
+                    
+                    
+                    
+                    
+                    .then(function (response)
+                    {
+                        var data = response.data;
 
-                });
-            };
+                        $scope.spring = data;
+                        $scope.testOk = true;
+
+                        $timeout(function ()
+                        {
+                            $scope.testOk = false;
+                        }, 3000);
+
+                    });
+        };
     }]);
 
 

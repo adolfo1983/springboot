77f68d8afd0f890c4d4aae5f1abf5eca6152091d
Lenovo <Lenovo@LENOVO-T420>
2017-08-31 12:13:57 +0200

    He incluido un archivo de lectura para probar el committ

--- src/main/resources/static/ng-controller/ng-app-controller-Search.js
+++ src/main/resources/static/ng-controller/ng-app-controller-Search.js
@@ -1,31 +1,35 @@
-app.controller('ng-app-controller-Search', ["$scope", "$http", "$timeout",
-    function ($scope, $http, $timeout)
-    {
-        $scope.search = function () {
-            var value = this.value;
-            $http.post('/searchItem',
-                    {
-                        nombre: value
-                    })
-                    
-                    
-                    
-                    
-                    
-                    .then(function (response)
-                    {
-                        var data = response.data;
-
-                        $scope.spring = data;
-                        $scope.testOk = true;
-
-                        $timeout(function ()
+app.controller('ng-app-controller-Search',
+        ['$scope', '$http', '$timeout', function ($scope, $http, $timeout)
+            {
+                var timer =
                         {
-                            $scope.testOk = false;
-                        }, 3000);
+                            search:
+                                    {
+                                        id: null,
+                                        ms: 750
+                                    }
+                        };
 
-                    });
-        };
-    }]);
+                $scope.searchArt = function (e)
+                {
+                    var value = e.target.value;
 
+                    $timeout.cancel(timer.search.id);
+                    timer.search.id = $timeout(function ()
+                    {
+                        $http.post('/searchItem',
+                                {
+                                    nombre: value
+                                })
+                                .then(function (response)
+                                {
+                                    var data = response.data;
+
+                                    $scope.articles = data;
+                                });
+
+                    }, timer.search.ms);
+                };
+
+            }]);
 
